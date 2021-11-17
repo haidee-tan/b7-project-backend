a36be80d12b061bb4bc61cbd13cc26ff97ecc1dc
haidee-tan <haideestan@gmail.com>
2021-11-17 14:43:27 +0800

    feat: users route backend

--- auth.js
+++ auth.js
@@ -18,7 +18,26 @@ const decodeToken = token => {
     return decoded;
 }
 
+const isAdmin = (req, res, next) => {
+    let token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, secretKey, (err, data) => {
+            if(err) {
+                res.send({auth: "cannot find token"});
+            }
+            if(data.role !== "admin") {
+                res.send({auth: "not admin"})
+            }
+            next();
+        });
+    }
+    else {
+        res.send({auth: "no token"});
+    };
+}
+
 module.exports = {
     createAccessToken,
-    decodeToken
+    decodeToken,
+    isAdmin,
 }
