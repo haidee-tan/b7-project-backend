63e0fa7900402b109789a36b6ba0e6758bdbb9c6
amlongalong <amlongalong@ymail.com>
2021-11-18 17:36:13 +0800

    Feat: multer for beneficiary and post

--- routes/beneficiaryRouter.js
+++ routes/beneficiaryRouter.js
@@ -3,40 +3,41 @@ const router = express.Router();
 const Beneficiary = require('../models/Beneficiary');
 const multer = require('multer')
 
-
-// MULTER CONFIG
-// SET FOR SINGLE UPLOAD
-// CAN SAVE UPLOAD BUT THRU POSTMAN, ALL PROPS REFLECTED IN THE FE BUT NOW THE ACTUAL IMAGE  
 const multerStorage = multer.diskStorage({
     destination: (req, file, next) => {
         next(null, './public');
     },
     filename: (req, file, next) => {
         const ext = file.mimetype.split('/')[1]
+        let random = (Math.random() + 1).toString(36).substring(2)
         next(null, Date.now() + '.' + ext )
     }
 })
 const upload = multer({ storage: multerStorage})
 
 
-
-
-router.get("/", upload.single('img'), (req,res) => {
+router.get("/", (req,res) => {
     Beneficiary.find({})
     .then(beneficiary => 
         res.send(beneficiary)
         )
 })
 
-router.post("/", upload.single('img'), (req,res) => {
-    let beneficiary = new Beneficiary(req.body);
+router.post("/", upload.single('photo'), (req,res) => {
+    let beneficiary = new Beneficiary();
+    beneficiary.name = req.body.name
+    beneficiary.address = req.body.address
+    beneficiary.contactNum = req.body.contactNum
+    beneficiary.description = req.body.description
+    beneficiary.website = req.body.website
+    beneficiary.photo = req.file.filename
     beneficiary.save()
     .then ( beneficiary => {
         res.send(beneficiary)
     })
 })
 
-router.put("/:id", (req,res) => {
+router.put("/:id", upload.single('photo'), (req,res) => {
     Beneficiary.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
     .then(data => {res.send(data)
     })
